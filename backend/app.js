c53877d298301c8619fb5693323b9b7e1deb1663
Evan Huntley <evanhuntley21@gmail.com>
2025-02-18 10:40:41 -0500

    updating for merge

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,38 @@
 const express = require('express')
 
-
+const port = 3000
 const app = express()
 
-app.listen(3000, (err)=>{
-    if(err){
-        console.log("ERROR")
+
+
+
+
+app.get('/', (req, res) => {
+    console.log("home page")
+    res.json({"sample data 1": {"attribute 1" : "value 1","attribute 2" : "value 2" }, "sample data 2": {"attribute 1" : "value 1","attribute 2" : "value 2" }})
+})
+
+app.listen(port, (err))
+.then(() => {
+    console.log("Server is running!")
+})
+.catch((err)=>{
+    console.log(err.message);
+})
+
+
+app.use((req, res, next) => {
+    let err = new Error("Unable to locate route")
+    err.status = 404
+    next()
+})
+
+app.use((err, req, res, next) => {
+    if(!err.status){
+        res.status = 404
+        res.json({"error": {"status": 404, "message": err.message}})
     }
-    else{
-        console.log("running on 3000")
-    }
+    let err = new Error("Critical server error")
+    res.status = 500
+    res.json({"error": {"status": 500, "message": err.message}})
 })
