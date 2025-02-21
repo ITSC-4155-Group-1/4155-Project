0b4ffc5c1dfdc07c7b396be155e58f7d8a4a6617
Evan Huntley <evanhuntley21@gmail.com>
2025-02-20 20:11:51 -0500

    fixed formatting for models

--- backend/model/bookingModel.js
+++ backend/model/bookingModel.js
@@ -4,7 +4,7 @@ const {Schema} = mongoose
 const bookingSchema = new Schema({
     buyerId: {type: Schema.Types.ObjectID, ref: 'User'},
     hostId: {type: Schema.Types.ObjectID, ref: 'User'},
-    venue_id: {type: Schema.Types.ObjectID, ref: 'Venue'},
+    venueId: {type: Schema.Types.ObjectID, ref: 'Venue'},
     bookingStartDate: {type: Date, required: [true, 'Venue availability start date is required']},
     bookingEndDate: {type: Date, required: [true, 'Venue availability end date is required']},
     numAttendees: {type: Number, required: [true, 'Number of attendees is required']}
--- backend/model/venueModel.js
+++ backend/model/venueModel.js
@@ -2,14 +2,14 @@ const mongoose = require ('mongoose')
 const {Schema} = mongoose
 
 const venueSchema = new Schema({
-    buyer_id: {type: Schema.Types.ObjectID, ref: 'User'},
-    venue_name: {type: String, required: [true, 'Venue name is required']},
+    buyerId: {type: Schema.Types.ObjectID, ref: 'User'},
+    venueName: {type: String, required: [true, 'Venue name is required']},
     description: {type: String, required: [true, 'Venue description is required']},
     host: {type: 'ObjectId', ref: 'User'},
     location: {type: String, required: [true, 'Venue location is required']},
     price: {type: Number, required: [true, 'Venue price is required'], minValue: [0.01, 'Price must be at least $0.01']},
-    availability_start_date: {type: Date, required: [true, 'Venue availability start date is required']},
-    availability_end_date: {type: Date, required: [true, 'Venue availability end date is required']},
+    availabilityStartDate: {type: Date, required: [true, 'Venue availability start date is required']},
+    availabilityEndDate: {type: Date, required: [true, 'Venue availability end date is required']},
     capacity: {type: Number, required: [true, 'Venue Capacity is required']},
     images: [{type: String, required: 'Image is required'}],
     ratingAvg: {type: Number},
