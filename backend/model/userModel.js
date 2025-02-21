a522e60534287a37f9534ef2cc6b19a8e0d379b4
Evan Huntley <evanhuntley21@gmail.com>
2025-02-20 20:14:57 -0500

    fixed formatting for models

--- backend/model/userModel.js
+++ backend/model/userModel.js
@@ -6,7 +6,7 @@ const userSchema = new Schema({
     lastName: {type: String, required: [true, 'last name is required']},
     email: {type: String, required: [true, 'email address is required'], unique: [true, 'this email address has been used'] },
     password: { type: String, required: [true, 'password is required'] },
-    image: {type: String, required: 'Image is required'}
+    image: {type: String, required: [true, 'Image is required']}
 })
 
 module.exports = mongoose.model("User", userSchema)
