585cb183b31a5d3fac08e8b786dda63483548eb3
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-02-27 17:17:22 -0500

    pushing main content below the navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,22 @@
 <script setup>
-  import NavBar from "./components/NavBar.vue";
-  import FooterComponent from "./components/FooterComponent.vue";
+    import NavBar from "./components/NavBar.vue";
+    import FooterComponent from "./components/FooterComponent.vue";
+    import { ref, onMounted } from 'vue';
+
+    const navHeight = ref(86);
+
+    onMounted(() => {
+        const navbar = document.querySelector('.navbar');
+        if (navbar) {
+            navHeight.value = navbar.offsetHeight;
+        }
+    });
 </script>
 
 <template>
   <div class="d-flex flex-column min-vh-100">
     <NavBar /> <!-- user is not logged in, will need a prop or something to the navbar change -->
-      <RouterView />
+      <RouterView :style="{ marginTop: navHeight + 'px' }"/>
     <FooterComponent />
   </div>
 </template>
