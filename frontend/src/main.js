98535e29be10a182df377cae33bc52662f30b367
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-02-27 17:18:19 -0500

    added VueDatePicker

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,11 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from "./router";
+
+import router from './router';
+
+import VueDatePicker from '@vuepic/vue-datepicker';
+import '@vuepic/vue-datepicker/dist/main.css'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
 
 createApp(App)
     .use(router)
+    .component('VueDatePicker', VueDatePicker)
     .mount('#app')
