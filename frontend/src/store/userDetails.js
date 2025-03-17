bcdf98db2ebed336fb348f56e6068f6a847cfc0a
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-03-17 11:27:47 -0400

    moved the logout functionality to the store to use it globally

--- frontend/src/store/userDetails.js
+++ frontend/src/store/userDetails.js
@@ -1,11 +1,18 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { useCookie } from 'vue-cookie-next'
+import axios from 'axios'
+import { useRouter } from 'vue-router'
+
 
 export const useUserStore = defineStore('user', () => {
     const user = ref({
         email: '',
         token: null,
     });
+    
+    const router = useRouter();
+    const cookies = useCookie();
 
     const setUser = (userData) => {
         user.value = userData;
@@ -15,7 +22,22 @@ export const useUserStore = defineStore('user', () => {
     const clearUser = () => {
         user.value = { email: '', token: '' }
         localStorage.removeItem('user');
+        cookies.removeCookie('authToken');
     }
 
-    return { user, setUser, clearUser }
+    const logout = async () => {
+        try {
+            if (cookies.getCookie('authToken')) {
+                await axios.get('http://localhost:3000/user/logout', { withCredentials: true });
+            }
+        } catch (error) {
+            console.error('Logout failed:', error.response?.data?.message || error);
+        }
+
+        clearUser();
+        router.push('/');
+        location.reload();
+    }
+
+    return { user, setUser, clearUser, logout }
 })
