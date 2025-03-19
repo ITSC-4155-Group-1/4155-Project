d09524fed13380e1415c366ca47e8bfdae7d55ac
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-03-18 23:17:33 -0400

    using the vue3-carousel component to display array of images

--- frontend/src/components/VenueCard.vue
+++ frontend/src/components/VenueCard.vue
@@ -1,34 +1,64 @@
 <script setup>
-import { ref } from 'vue';
+    import { ref, computed, toRefs } from 'vue';
+    import { Carousel, Slide, Navigation } from 'vue3-carousel'
+    import 'vue3-carousel/carousel.css'
 
-defineProps({
-    venue: Object,
-});
+    const props = defineProps({
+        venue: Object,
+    });
 
-let isFilled = ref(false);
+    const { venue } = toRefs(props)
+    const isFilled = ref(false);
+    // console.log(venue.value)
+    const images = computed(() => venue.value.image ?? []);
 
-const toggleIsFilled = () => {
-    isFilled.value = !isFilled.value;
-};
+    const toggleIsFilled = () => {
+        // will make a backend call to favorite it
+        isFilled.value = !isFilled.value;
+    };
+
+    const goToVenue = (venueName) => {
+        // will make a backend call to go to the venue page
+        router.push(`/venues/${venueName}`)
+    };
+
+    const carouselConfig = {
+        height: 225,
+        itemsToShow: 1,
+        wrapAround: true,
+    }
 </script>
 
 <template>
     <div class="card border-0 background hover-effect p-2">
-        <router-link :to="`/venues/${venue.venue_name}`" class="text-decoration-none">
-            <div class="image-container position-relative">
-                <span class="position-absolute top-0 start-0 rating p-2"> <!-- rating badge -->
-                    <span> <!-- star icon -->
-                        <svg width="23" height="23" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
-                            <polygon points="7,1 8.54,5 13,5 9.23,7.95 10.77,12 7,9.5 3.23,12 4.77,7.95 1,5 5.46,5"
-                                fill="none" stroke="#FFC107" stroke-width="1"/>
-                        </svg>
-                    </span>
-                    {{ venue.rating }} / 5.0 
+        <div class="image-container position-relative">
+            <span class="position-absolute top-0 start-0 rating p-2"> <!-- rating badge -->
+                <span> <!-- star icon -->
+                    <svg width="23" height="23" viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
+                        <polygon points="7,1 8.54,5 13,5 9.23,7.95 10.77,12 7,9.5 3.23,12 4.77,7.95 1,5 5.46,5"
+                        fill="none" stroke="#FFC107" stroke-width="1"/>
+                    </svg>
                 </span>
-                <!-- need to convert image array to image carousel -->
-                <img :src="venue.image[1]" class="card-img-top" alt="Image of venue">
-                <span class="badge position-absolute bottom-0 end-0 m-2 capacity">{{ venue.capacity }} people </span>
-            </div>
+                {{ venue.rating }} / 5.0 
+            </span>
+            <Carousel v-bind="carouselConfig">
+                <Slide v-for="image in images" :key="image">
+                    <img
+                        :src="image"
+                        class="card-img-top"
+                        alt="Venue Images"
+                        loading="lazy"
+                        @click="goToVenue(venue.venue_name)"
+                    >
+                </Slide>
+                
+                <template #addons>
+                    <Navigation class="mx-1" />
+                </template>
+            </Carousel>
+            <span class="badge position-absolute bottom-0 end-0 m-2 capacity">{{ venue.capacity }} people </span>
+        </div>
+        <router-link :to="`/venues/${venue.venue_name}`" class="text-decoration-none">
             <div class="mt-3 d-flex flex-column">
                 <h5 class="card-title">{{ venue.location }}</h5>
                 <p class="card-text">6 Miles Away</p>  <!-- Hardcoded distance, will replace with Google API -->
@@ -66,10 +96,14 @@ const toggleIsFilled = () => {
         cursor: pointer;
     }
 
+    .carousel {
+        --vc-nav-background: rgba(255, 255, 255, 0.7);
+        --vc-nav-border-radius: 100%;
+    }
+
     .image-container {
         width: 100%;
         height: 225px;
-        overflow: hidden;
     }
 
     .image-container img {
@@ -81,6 +115,7 @@ const toggleIsFilled = () => {
     .capacity {
         font-size: 14px;
         background-color: var(--primary);
+        z-index: 10;
     }
     
     .rating {
@@ -89,7 +124,8 @@ const toggleIsFilled = () => {
         color: var(--highlight);
         font-size: 16px;
         font-weight: bolder;
-        border-radius: 0 0 0.5rem 0;
+        border-radius: 5px 0 0.5rem 0;
+        z-index: 10;
     }
 
     .background {
