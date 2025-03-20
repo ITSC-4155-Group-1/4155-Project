872d787089b215ab2b4b8da7cbc715c8b55c83b4
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-03-19 23:38:20 -0400

    sending the image in the token but is null by default

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -16,6 +16,7 @@ exports.login = (req, res, next) => {
                 req.session.user = user._id;
                 req.session.firstName = user.firstName;
                 req.session.lastName = user.lastName;
+                req.session.image = null;
                 res.json({ success: `Login successful`, token: req.session });
             } else {
                 return res.status(400)
