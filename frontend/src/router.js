7c813c1a807ee1574c66804c1cbf49cf6708fd80
Denny981 <dennyzhang19@gmail.com>
2025-03-21 13:05:06 -0400

    Routing for ErrorPage

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -7,6 +7,7 @@ import CreateVenue from "./components/CreateVenue.vue";
 import ShoppingCart from "./components/ShoppingCart.vue";
 import NotifPage from "./components/NotifPage.vue"
 import Messages from "./components/Messages.vue";
+import ErrorPage from "./components/ErrorPage.vue";
 
 const routes = [
     { path: '/', component: LandingPage },
@@ -16,6 +17,7 @@ const routes = [
     { path: '/cart', component: ShoppingCart },
     { path: '/notifications', component: NotifPage },
     { path: '/messages', component: Messages },
+    { path: '/error', component: ErrorPage },
 ];
 
 const router = createRouter({
