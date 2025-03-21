2d45af4bbf6247f0f325cc8b1633edb353f16e7d
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-03-20 22:35:59 -0400

    cleaned up css

--- frontend/src/components/Messages.vue
+++ frontend/src/components/Messages.vue
@@ -2,29 +2,17 @@
     import { ref } from 'vue';
     import { parseUser } from "../utils/userUtils"
     
-    const showModal = ref(false);
     const activeSection = ref('personal-info');
-    const updatePasswordDiv = ref(false);
     const user = parseUser();
 
     const setActiveSection = (section) => {
         activeSection.value = section;
     };
 
-    const toggleUpdatePasswordDiv = () => {
-        updatePasswordDiv.value = !updatePasswordDiv.value;
-    }
-
-    const updatePassword = async () => {
-        // backend call to update password
-        updatePassword.value = false;
-    }
-
-    //This is just sample. You can change this 
     const newMessage = ref('');
     const messages = ref([
-        { sender: 'Santiago', text: 'Hey, how are you?' },
-        { sender: 'Me', text: 'I’m good, what about you?' }
+        { sender: 'Santiago', text: "Hey, how are you?", opened: true },
+        { sender: 'Me', text: "I'm good, what about you?", opened: false }
     ]);
 
     const sendMessage = () => {
@@ -38,54 +26,106 @@
 
 <template>
     <div>
-        <div class="messages-container">
-            <div class="sidebar">
-                <div>
-                    <div class="sidebar-header">
+        <div class="d-flex gap-2">
+            <div
+                class="sidebar flex-shrink-0 overflow-y-auto d-flex flex-column justify-content-start w-25 p-4"
+            >
+                <div class="d-flex flex-column gap-2">
+                    <div class="d-flex align-items-center justify-content-between">
                         <h2 class="sidebar-title">Messages</h2>
-                        <button class="search-btn">
-                            <img src="/images/search.png" alt="Search" />
-                        </button>
+                        <img
+                            class="searchIcon bg-none"
+                            src="/images/search.png"
+                            alt="Search"
+                            role="button"
+                        />
                     </div>
                     
-                    <div class="filter-buttons">
-                        <button :class="{ active: activeSection === 'all-messages' }" @click="setActiveSection('all-messages')">
-                            All
+                    <div class="d-flex align-items-center gap-3 mb-2">
+                        <button
+                            class="px-3 py-2 rounded border-0 filterButtons"
+                            role="button"
+                            :class="{ active: activeSection === 'all-messages' }"
+                            @click="setActiveSection('all-messages')">
+                            <span>All</span>
                         </button>
-                        <button :class="{ active: activeSection === 'unread-messages' }" @click="setActiveSection('unread-messages')">
-                            Unread
+                        <button
+                            class="px-3 py-2 rounded border-0 filterButtons"
+                            role="button"
+                            :class="{ active: activeSection === 'unread-messages' }"
+                            @click="setActiveSection('unread-messages')">
+                            <span>Unread</span>
                         </button>
                     </div>
                 </div>
-                <!-- Here is the code for each person's messages -->
-                <ul class="sidebar-menu">
-                    <li :class="{ active: activeSection === 'Person 1' }" @click="setActiveSection('Person 1')">
-                        <img class="avatar" src="/images/profile_4.jpeg" alt="User Avatar"> Santiago
+
+                <!-- each person's messages -->
+                <ul class="sidebar-menu list-style-none m-0 p-0">
+                    <li
+                        class="d-flex align-items-center p-3"
+                        role="button"
+                        :class="{ active: activeSection === 'Person 1' }"
+                        @click="setActiveSection('Person 1')"
+                    >
+                        <img class="avatar" src="/images/profile_4.jpeg" alt="User Avatar">
+                        <span>Santiago</span>
                     </li>
-                    <li :class="{ active: activeSection === 'Person 2' }" @click="setActiveSection('Person 2')">
-                        <img class="avatar" src="/images/profile_4.jpeg" alt="User Avatar"> Lil Guy
+                    <li
+                        class="d-flex align-items-center p-3"
+                        role="button"
+                        :class="{ active: activeSection === 'Person 2' }"
+                        @click="setActiveSection('Person 2')"
+                    >
+                        <img class="avatar" src="/images/profile_4.jpeg" alt="User Avatar">
+                        <span>Lil Guy</span>
                     </li>
-                    <li :class="{ active: activeSection === 'Person 3' }" @click="setActiveSection('Person 3')">
-                        <img class="avatar" src="/images/profile_4.jpeg" alt="User Avatar"> \._./
+                    <li
+                        class="d-flex align-items-center p-3"
+                        role="button"
+                        :class="{ active: activeSection === 'Person 3' }"
+                        @click="setActiveSection('Person 3')"
+                    >
+                        <img class="avatar" src="/images/profile_4.jpeg" alt="User Avatar">
+                        <span>\._./</span>
                     </li>
                 </ul>
             </div>
-            <!-- Each individual persons texting chat, Only Person 1 works because it is hard coded. -->
-            <div v-if="activeSection === 'Person 1'" class="chat-profile">
-                <div class="chat-image"> 
-                    <img class="avatar" src="/images/profile_4.jpeg" alt="User Avatar"><h4>Santiago</h4>
-                </div>
-                <div class="message-inbox">
-                    <div class="messages">
-                        <div v-for="(message, index) in messages" :key="index" :class="['message', message.sender === 'Me' ? 'sent' : 'received']">
+
+            <!-- only need this 'template'; information needs to update based on the 'activeSection' variable -->
+            <div 
+                v-if="activeSection === 'Person 1'"
+                class="chat-box d-flex flex-column w-100 flex-fill"
+            >
+                <div
+                    class="message-inbox d-flex flex-column flex-fill overflow-y-auto p-4"
+                >
+                    <div
+                        class="d-flex flex-column g-1 pb-2 flex-fill overflow-y-auto"
+                    >
+                        <div
+                            v-for="(message, index) in messages"
+                            :key="index"
+                            :class="['message', message.sender === 'Me' ? 'sent' : 'received']"
+                        >
                             {{ message.text }}
                         </div>
                     </div>
 
-                    <!-- Input Box -->
-                    <div class="texting-box">
-                        <input v-model="newMessage" @keyup.enter="sendMessage" type="text" placeholder="Type a message..." />
-                        <button @click="sendMessage">Send</button>
+                    <div class="d-flex gap-3 py-3">
+                        <input
+                            v-model="newMessage"
+                            @keyup.enter="sendMessage"
+                            type="text"
+                            placeholder="Type a message..." 
+                            class="messageInput w-100 p-2 border-0 rounded"
+                        />
+                        <button
+                            class="submitMessageBtn rounded border-0 px-3 py-2"
+                            @click="sendMessage"
+                            role="button"
+                        >
+                            Send
+                        </button>
                     </div>
                 </div>
             </div>
@@ -94,231 +134,102 @@
 </template>
 
 <style scoped>
-.messages-container {
-    display: flex;
-    padding: 5px;
-    border-top: 2px solid #ddd; 
-}
+    .searchIcon {
+        height: 25px;
+        width: 25px;
+    }
 
-.sidebar {
-    width: 25%;
-    padding: 30px;
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    border-right: 2px solid #ddd; 
-    height: 90vh; 
-    overflow-y: auto;
-    flex-shrink: 0;
-}
+    .sidebar::-webkit-scrollbar {
+        width: 8px;
+    }
 
-.sidebar-header {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 5px;
-}
+    .sidebar::-webkit-scrollbar-thumb {
+        background: #ccc;
+        border-radius: 10px;
+    }
 
-.search-btn {
-    background: none;
-    border: none;
-    cursor: pointer;
-    color: #3F51B5;
-    transition: color 0.3s;
-    display: flex;
-    align-items: center;
-    margin-bottom: 25px; 
-    height: 25px;
-    width: 25px;
-}
+    .sidebar-title {
+        color: var(--primary);
+    }
 
-.search-btn img{
-    height: 25px;
-    width: 25px;
-}
+    .filterButtons {
+        transition: background 0.3s;
+    }
 
-.sidebar::-webkit-scrollbar {
-    width: 8px;
-}
+    .filterButtons.active {
+        background: #dbeafe;
+        font-weight: bold;
+    }
 
-.sidebar::-webkit-scrollbar-thumb {
-    background: #ccc;
-    border-radius: 10px;
-}
+    .filterButtons:hover {
+        background: #c5e0ff;
+    }
 
-.sidebar-title {
-    font-size: 32px;
-    margin-bottom: 20px;
-    color: #3F51B5;
-}
+    .sidebar-menu li {
+        transition: background 0.3s;
+        border-radius: 20px; 
+    }
 
-.sidebar-menu {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-}
+    .sidebar-menu li.active {
+        background: #dbeafe;
+        font-weight: bold;
+    }
 
-.filter-buttons {
-    display: flex;
-    gap: 10px;
-    margin-bottom: 15px;
-    align-items: center;
-}
+    .sidebar-menu li:hover {
+        background: #e7f0ff;
+    }
 
-.filter-buttons button {
-    padding: 10px 15px;
-    border-radius: 20px; 
-    border: none;
-    cursor: pointer;
-    border-radius: 5px;
-    transition: background 0.3s;
-}
+    .avatar {
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        margin-right: 20px;
+    }
 
-.filter-buttons button.active {
-    background: #dbeafe;
-    font-weight: bold;
-}
+    .chat-box {
+        height: 90vh;
+    }
 
-.filter-buttons button:hover {
-    background: #c5e0ff;
-}
+    .message-inbox {
+        background: #f9f9f9;
+    }
 
-.sidebar-menu {
-    list-style: none;
-    padding: 0;
-}
+    .message {
+        padding: 10px 15px;
+        border-radius: 7px;
+        max-width: 50%;
+        word-wrap: break-word;
+        margin: 3px 0;
+    }
 
-.sidebar-menu li {
-    padding: 15px 15px;
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    transition: background 0.3s;
-    border-radius: 20px; 
-}
+    .message.received {
+        background: #e0e0e0;
+        align-self: flex-start;
+    }
 
-.sidebar-menu li.active {
-    background: #dbeafe;
-    font-weight: bold;
-}
+    .message.sent {
+        background: var(--primary);
+        color: white;
+        align-self: flex-end;
+        margin-right: 5px;
+    }
 
-.sidebar-menu li:hover {
-    background: #e7f0ff;
-}
+    .messages-container:has(.chat-profile:empty) {
+        justify-content: space-between;
+    }
 
-.avatar {
-    width: 50px;
-    height: 50px;
-    border-radius: 50%;
-    margin-right: 20px;
-}
+    .messageInput { 
+        outline: none;
+        background: var(--background);
+    }
 
-.chat-profile {
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-    height: 90vh;
-    flex-grow: 1;
-}
+    .submitMessageBtn {
+        background: var(--primary);
+        color: white;
+        transition: 0.3s;
+    }
 
-.chat-image{
-    display: flex;
-    flex-direction: row;
-    padding: 20px 20px 20px 20px;
-    width: 100%;
-    align-items: center;
-    gap: 10px;
-    border-bottom: 2px solid #ddd; 
-}
-
-.chat-image h4 {
-    margin: 0;  
-    line-height: 1;  
-    display: flex;
-    align-items: center;
-}
-
-.chat-image {
-    display: flex;
-    flex-direction: row;
-    padding: 20px;
-    width: 100%;
-    align-items: center;
-    gap: 10px;
-    border-bottom: 2px solid #ddd;
-
-}
-
-.message-inbox {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    overflow-y: auto;
-    padding: 20px;
-    background: #f9f9f9;
-}
-
-.messages {
-    flex-grow: 1;
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    padding-bottom: 20px;
-    flex: 1; 
-    overflow-y: auto;
-}
-
-.message {
-    padding: 10px 15px;
-    border-radius: 15px;
-    max-width: 70%;
-    word-wrap: break-word;
-}
-
-.message.received {
-    background: #e0e0e0;
-    align-self: flex-start;
-}
-
-.message.sent {
-    background: #3F51B5;
-    color: white;
-    align-self: flex-end;
-}
-
-.messages-container:has(.chat-profile:empty) {
-    justify-content: space-between;
-}
-
-.texting-box {
-    display: flex;
-    padding: 10px;
-    background: white;
-    border-top: 2px solid #ddd;
-    margin-top: auto;
-}
-
-.texting-box input {
-    flex-grow: 1;
-    padding: 10px;
-    border: none;
-    border-radius: 20px;
-    outline: none;
-    background: #f0f0f0;
-}
-
-.texting-box button {
-    padding: 10px 15px;
-    margin-left: 10px;
-    border: none;
-    border-radius: 20px;
-    background: #3F51B5;
-    color: white;
-    cursor: pointer;
-    transition: 0.3s;
-}
-
-.texting-box button:hover {
-    background: #283593;
-}
+    .submitMessageBtn:hover {
+        background: #283593;
+    }
 </style>
