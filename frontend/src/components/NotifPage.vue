315b16986f42c7b3df909961553a2638e7a67a87
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-03-23 10:31:16 -0400

    touched up css and applied a fading effect when the x button is clicked

--- frontend/src/components/NotifPage.vue
+++ frontend/src/components/NotifPage.vue
@@ -2,29 +2,34 @@
     import { notifications } from '../../../mockdata';
     import { ref } from "vue"
 
-    defineProps({
-        notif: Object
-    });
-
     const notifList = ref(notifications);
 
-    function remove(event){
-        //Remove notification from display
-        const list_item = event.target.parentNode.parentNode;
-        list_item.classList.add("d-none");
+    const remove = (index) => {
+    // Trigger fade-out by adding a class
+    notifList.value[index].isFading = true;
 
-        //TODO: Add code to remove notif from database
-    }
+    // Wait for animation to finish before removing
+    setTimeout(() => {
+        notifList.value.splice(index, 1);
+        // TODO: Add code to remove notif from database
+    }, 500); // Matches the CSS transition duration
+};
 </script>
 
 <template>
-    <div class="notifPage">
-        <h1>Notifications</h1>
-        <ul class="listgroup">
-            <li v-for="notif in notifList">
+    <div class="notifContainer mx-auto">
+        <h1 class="pb-2">Notifications</h1>
+        <ul class="list-group list-unstyled">
+            <li
+                v-for="(notif, idx) in notifList"
+                :key="idx"
+                class="mb-4 hover-effect py-3 px-1 rounded m-0"
+                :class="{ 'fade-out': notif.isFading }"
+                role="button"
+            >
                 <div class="d-flex justify-content-between">
-                    <p>{{ notif.notification }}</p>
-                    <button class="removeButton" @click="remove">x</button>
+                    <p class="m-0">{{ notif.notification }}</p>
+                    <button class="border-0 bg-transparent me-3" @click="remove(idx)">&#10005;</button>
                 </div>
             </li>
         </ul>
@@ -32,29 +37,26 @@
 </template>
 
 <style scoped>
-    .notifPage {
-        margin: auto 20% auto 15%;
+    .notifContainer {
+        width: 80%;
     }
 
     h1 {
         color: var(--primary);
-        padding-bottom: .5rem;
     }
 
-    li {
-        list-style-type: none;
-        margin-bottom: 1rem;
+    .hover-effect {
+        transition: 0.3s ease-in-out;
     }
 
-    ul {
-        padding: 0;
+    .hover-effect:hover {
+        background-color: rgba(0, 0, 0, 0.05);
+        box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+        scale: 1.01;
     }
 
-    .removeButton {
-        border: none;
-    }
-    
-    p {
-        margin-bottom: 0;
+    .fade-out {
+        opacity: 0;
+        transition: opacity 0.5s ease;
     }
 </style>
