bab6336ac4a350e8ad989d1c575a9f89f4bf5b72
Evan Huntley <evanhuntley21@gmail.com>
2025-04-02 12:37:25 -0400

    working on room creation

--- backend/controllers/messageController.js
+++ backend/controllers/messageController.js
@@ -12,3 +12,36 @@ exports.getUsersToMessage = (req, res, next) =>{
   })
   .catch(err => next(err))
 }
+
+exports.createRoom = (io) =>{
+  io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+    // data = socket.handshake.query
+    const room = "test"
+    //data is an object containing:
+    //receiver: Str
+    socket.on("joinRoom", (data) => {
+      let currUser = req.session.user
+      let roomId = [currUser, reciever].sort()
+      roomId = roomId[0].concat(roomId[1])
+      socket.join(roomId)
+      Promise.all([userModel.findById(currUser), userModel.findById(reciever), messageModel.find({senderId: currUser, receiverId: reciever})])
+      .then((usersAndMessages) =>{
+        [sender, reciever, messages] = usersAndMessages
+      })
+    })
+    
+  })
+  // Function to send message
+  socket.on('message', (msg)=>{
+    io.to(room).emit('messageBack', `Message recieved!`)
+    io.to(room).emit('messageBack', `Hello, ${socket.id}, here is your message: ${msg}`)
+  })
+  socket.on('disconnect', () => {
+    console.log('a user disconnected');
+  });
+  
+  // Function to disconnect
+  
+  
+}
