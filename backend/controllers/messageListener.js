a92c87510d3cf0e77a892c22f35f19a874d4ffca
Evan Huntley <evanhuntley21@gmail.com>
2025-04-03 15:55:07 -0400

    setup message listener, may have bugs

--- backend/controllers/messageListener.js
+++ backend/controllers/messageListener.js
@@ -1,21 +1,43 @@
+const messageModel = require('../model/messageModel')
+const userModel = require('../model/userModel')
 module.exports = (io) =>{
-    io.on('connection', (socket) => {
-      console.log('A user connected:', socket.id);
-      data = socket.handshake.query
-      const room = "test"
-      socket.join(room)
-      socket.on('message', (msg)=>{
-        io.to(room).emit('messageBack', `Message recieved!`)
-        io.to(room).emit('messageBack', `Hello, ${socket.id}, here is your message: ${msg}`)
+  let roomId = ''
+  io.on('connection', (socket) => {
+    //data is an object containing:
+    //receiver: Str
+    //currUser: Str
+    socket.on("joinRoom", (data) => {
+      let currUser = data.currUser
+      let reciever = data.reciever
+      roomId = [currUser, reciever].sort()
+      roomId = roomId[0].concat(roomId[1])
+      socket.join(roomId)
+      Promise.all([userModel.findById(currUser), userModel.findById(reciever), messageModel.find({senderId: currUser, receiverId: reciever})])
+      .then((usersAndMessages) =>{
+        [sender, reciever, messages] = usersAndMessages
+        io.to(roomId).emit(usersAndMessages)
       })
-      socket.on('disconnect', () => {
-        console.log('a user disconnected');
-      });
-      
+      .catch(err => next(err))
     })
-    // Function to send message
     
-    // Function to disconnect
-    
-    
-  }
+  })
+  // Function to send message
+  socket.on('message', (data)=>{
+    //data.message: Str: The message the user sent
+    //data.senderId: Objectid
+    //data.receiverId: Objectid
+    message = new messageModel(data)
+    message.save()
+    .then((message) =>{
+      io.to(roomId).emit('messageToClient', data)
+    })
+    .catch(err => next(err))
+  })
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  });
+  
+  // Function to disconnect
+  
+  
+}
