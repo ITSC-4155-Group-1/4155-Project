b46e86a3df8b692eaee7ff2c3dcad49579ad8fac
Santiago Orozco Buri <sorozcob@charlotte.edu>
2025-04-11 11:48:01 -0400

    reusable component to display bookings

---
+++ frontend/src/components/Booking.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { ref, toRefs } from 'vue';
+
+    const props = defineProps({
+        venue: Object,
+        booking: Object
+    });
+
+    const { venue, booking } = toRefs(props);
+</script>
+
+<template>
+    <h3>Venue Info:</h3>
+    <pre>{{ venue }}</pre>
+    <h3>Booking Info:</h3>
+    <pre>{{ booking }}</pre>
+</template>
+
+<style scoped>
+
+</style>
